de9fcdb891213814d964bc4552d334ece1afbd96
hashem sheikhypour <sh.sheikhypour@gmail.com>
2023-03-03 14:15:35 +0330

    add getX package

--- getx_routing_sample/lib/main.dart
+++ getx_routing_sample/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_routing_sample/routes/app_routes.dart';
+import 'package:getx_routing_sample/screens/not_found.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +12,17 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'GetX Routing Sample',
+      unknownRoute: GetPage(
+        name: Routes.notfound,
+        page: () => const UnknownRoutePage(),
+      ),
+      initialRoute: '/',
+      getPages: appRoutes(),
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
       ),
     );
   }
