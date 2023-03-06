d72bcd59585557e253ab8ecc879fb583c4003320
hashem sheikhypour <sh.sheikhypour@gmail.com>
2023-03-06 20:23:41 +0330

    create project

---
+++ counter_bloc_traning/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Counter App Bloc',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(),
+    );
+  }
+}
